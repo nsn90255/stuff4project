4dec756a4159869433ab125f499f847a4187e474
nsn90255 <nsn90255@gmail.com>
2025-04-02 18:27:20 +0200

    Install dbt

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-apt install -y firefox krita kdenlive audacity vlc gimp libreoffice thunderbird inkscape blender git vim mpv openvpn chromium flameshot 
+apt install -y firefox krita kdenlive audacity vlc gimp libreoffice thunderbird inkscape blender git vim mpv openvpn chromium flameshot wget
+
+wget https://github.com/nsn90255/project/releases/download/1.0/dbt.deb 
+
+dpkg -i dbt.deb
