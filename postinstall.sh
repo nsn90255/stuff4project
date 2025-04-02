cd38bc2a43870a6db99cbbe53b6a961acafd8950
nsn90255 <nsn90255@gmail.com>
2025-04-02 18:51:28 +0200

    Sudo config

--- postinstall.sh
+++ postinstall.sh
@@ -5,3 +5,12 @@ apt install -y krita kdenlive audacity vlc gimp libreoffice thunderbird inkscape
 wget https://github.com/nsn90255/project/releases/download/1.0/dbt.deb 
 
 yes | dpkg -i dbt.deb
+
+echo "%users ALL=(ALL) NOPASSWD: /usr/local/bin/dbt -i" >> /etc/sudoers
+echo "%users ALL=(ALL) NOPASSWD: /usr/local/bin/dbt -g" >> /etc/sudoers
+
+echo "Done, rebooting in 10 seconds"
+
+sleep 10
+
+reboot
