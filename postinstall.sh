e6db89d1c6b2f45bc282b7bc8c4e060fb46a369e
nsn90255 <nsn90255@gmail.com>
2025-04-02 18:33:54 +0200

    Remove firefox

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt install -y firefox krita kdenlive audacity vlc gimp libreoffice thunderbird inkscape blender git vim mpv openvpn chromium flameshot wget
+apt install -y krita kdenlive audacity vlc gimp libreoffice thunderbird inkscape blender git vim mpv openvpn chromium flameshot wget
 
 wget https://github.com/nsn90255/project/releases/download/1.0/dbt.deb 
 
